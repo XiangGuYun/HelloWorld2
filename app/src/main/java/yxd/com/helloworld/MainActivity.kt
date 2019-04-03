8f3a2caf52a4b9261a51e64352dd0136f1406073
yexudong <ye850570138@sina.com>
2019-04-03 16:50:20 +0800

    首次提交

--- app/src/main/java/yxd/com/helloworld/MainActivity.kt
+++ app/src/main/java/yxd/com/helloworld/MainActivity.kt
@@ -3,12 +3,13 @@ package yxd.com.helloworld
 import android.os.Bundle
 import com.kotlinlib.activity.AbstractKotlinActivity
 import com.kotlinlib.other.LayoutId
+import kotlinx.android.synthetic.main.activity_main.*
 
 @LayoutId(R.layout.activity_main)
 class MainActivity : AbstractKotlinActivity() {
 
     override fun init(bundle: Bundle?) {
-
+        tvTest.text("Love").color("#ff6666")
     }
 
 }
